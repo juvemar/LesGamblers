21507efdd07311d02c712053b5b4a30ed6ba8606
juvemar <martoo.atanasov@gmail.com>
2016-07-05 18:05:04 +0300

    Gamblers points can be updated by admin.

--- LesGamblers.Web/Controllers/GamblersController.cs
+++ LesGamblers.Web/Controllers/GamblersController.cs
@@ -45,5 +45,53 @@
 
             return this.View(gamblersModel);
         }
+
+        [HttpGet]
+        public ActionResult UpdateGamblerPoints()
+        {
+            var model = new UpdateGamblerViewModel();
+            model.AllGamblers = new List<SelectListItem>();
+
+            var allGamblers = this.gamblers.GetAll().ToList();
+            foreach (var gambler in allGamblers)
+            {
+                model.AllGamblers.Add(new SelectListItem
+                    {
+                        Text = gambler.FirstName + " " + gambler.LastName,
+                        Value = gambler.UserName
+                    });
+            }
+
+            return this.View(model);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateGamblerPoints(UpdateGamblerViewModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                model.AllGamblers = new List<SelectListItem>();
+
+                var allGamblers = this.gamblers.GetAll().ToList();
+                foreach (var current in allGamblers)
+                {
+                    model.AllGamblers.Add(new SelectListItem
+                    {
+                        Text = current.FirstName + " " + current.LastName,
+                        Value = current.UserName
+                    });
+                }
+                return this.View(model);
+            }
+
+            var gambler = this.gamblers.GetByUsername(model.UserName).FirstOrDefault();
+            if (gambler != null)
+            {
+                var updatedGambler = AutoMapper.Mapper.Map<UpdateGamblerViewModel, LesGamblers.Models.Gambler>(model);
+                this.gamblers.UpdateGambler(updatedGambler, gambler.Id);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
